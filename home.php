96727220c88f209c3da251c636985de6f1673ee1
Muhammad Sholeh <mskjdn@gmail.com>
2025-12-14 16:50:59 +0700

    fix home

--- home.php
+++ home.php
@@ -15,16 +15,16 @@
             <span class="logo-text">Koperasi<span class="logo-highlight">Ku</span></span>
         </div>
         <nav>
-            <a href="#">Tentang Kami</a>
+            <a href="#layanan">Layanan</a>
             <a href="admin.php" class="admin">Admin</a>
         </nav>
     </header>
 
     <section class="hero">
         <div class="hero-text">
-            <h1 style="font-size:48px; font-weight:800; line-height:1.1;">
-                Kami siap membantu <br>
-                <span style="color:#f4b400;">Finansial Anda</span>
+            <h1>
+                Kami Siap Membantu <br>
+                <span>Finansial Anda</span>
             </h1>
             <p>Kami adalah wadah solusi finansial dan pemasaran terpadu bagi seluruh anggota.
                 Sebagai Koperasi, kami tidak hanya menjadi mitra terpercaya dalam pengelolaan investasi Anda yang
@@ -33,37 +33,14 @@
                 Kami menyatukan produk dan jasa anggota, membuka akses pasar yang lebih luas, dan meningkatkan daya
                 saing kolektif. Tujuannya sederhana: pertumbuhan ekonomi yang merata dan berkelanjutan bagi setiap
                 anggota.</p>
-            <button>Daftar Sekarang</button>
         </div>
-        <div style="position: relative; width: 500px; height: 450px;">
-            <img src="assets/img/image1.png" style="
-            position:absolute;
-            top: 20px;
-            left: 60px;
-            width: 230px;
-         ">
-            <img src="assets/img/image2.png" style="
-            position:absolute;
-            top: 300px;
-            left: 130px;
-            width: 230px;
-         ">
-            <img src="assets/img/image3.png" style="
-            position:absolute;
-            top: 0;
-            left: 330px;
-            width: 180px;
-         ">
-
+        <div class="hero-img">
+            <img src="assets/img/image1.png">
+            <img src="assets/img/image2.png">
+            <img src="assets/img/image3.png">
         </div>
     </section>
-    <h2 style="
-    text-align:center;
-    font-size:32px;
-    font-weight:800;
-    line-height:1.2;
-    margin-top:60px;
-">
+    <h2 id="layanan">
         <span style="color:#222;">Apa Saja yang Ada di</span><br>
         <span style="color:#f4b400;">Koperasi Kami</span>
     </h2>
@@ -71,57 +48,33 @@
     <section class="features">
         <div>
             <div class="feature-box"></div>
-            <p style="text-align:center;">Barang Tersedia</p>
+            <p>Barang Tersedia</p>
         </div>
         <div>
             <div class="feature-box"></div>
-            <p style="text-align:center;">Tata Cara Peminjaman</p>
+            <p>Tata Cara Peminjaman</p>
         </div>
         <div>
             <div class="feature-box"></div>
-            <p style="text-align:center;">Info Pelatihan</p>
+            <p>Info Pelatihan</p>
         </div>
     </section>
 
-    <section style="
-    display:flex;
-    align-items:center;
-    justify-content:space-between;
-    padding:60px 40px;
-    background:linear-gradient(to right, white 60%, rgba(255,255,255,0));
-">
-        <div style="max-width:50%;">
-            <h2 style="font-size:48px; font-weight:800; margin:0 0 15px; color:#222;">
+    <section class="pelatihan-section">
+        <div class="pel-text">
+            <h2>
                 Tunggu Apalagi?
             </h2>
 
-            <p style="font-size:36px; line-height:1.3; font-weight:700; margin:0 0 30px; color:#222;">
+            <p>
                 Ikuti Pelatihan Kami dan<br>
-                Dapatkan <span style="color:#f4b400;">Sertifikat</span> Anda!
+                Dapatkan <span>Sertifikat</span> Anda!
             </p>
 
-            <button style="
-            background:#f4b400;
-            border:none;
-            padding:20px 50px;
-            font-size:26px;
-            font-weight:700;
-            border-radius:12px;
-            cursor:pointer;
-            color:#000;
-        ">
-                Daftar Sekarang
-            </button>
+            <span>(Hubungi admin untuk mendaftar)</span>
         </div>
 
-        <div style="max-width:50%; display:flex; justify-content:center;">
-            <img src="assets/img/image4.png" style="
-            width:120%;
-            max-width:600px;
-            opacity:0.45;
-            object-fit:cover;
-         ">
-        </div>
+        <img src="assets/img/image4.png">
     </section>
 
     <footer>
